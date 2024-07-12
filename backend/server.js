8338317c0452d08e2dd1603dd4b5af28fb830d18
Sujal <sujalkesharwani220@gmail.com>
2024-07-12 19:02:47 +0530

    test

--- backend/server.js
+++ backend/server.js
@@ -11,31 +11,32 @@ import { app, server } from "./socket/socket.js";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.set('strict routing', false);
-
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true,
-  allowedHeaders: ['Authorization', 'Content-Type'],
-}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    allowedHeaders: ["Authorization", "Content-Type"],
+  })
+);
 
 app.get("/", (req, res) => {
   res.send(req.cookies);
 });
 
-
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
-app.use("/api/users", (req,res,next) => {
-  console.log("middleware: ",req.cookies);
-  next();
-},userRouter);
-
+app.use(
+  "/api/users",
+  (req, res, next) => {
+    console.log("middleware: ", req.cookies);
+    next();
+  },
+  userRouter
+);
 
 server.listen(PORT, async () => {
   console.log(`Server is runs on http://localhost:${PORT}`);
