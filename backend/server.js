b99e0a92fb7497b179330fd91552ae4fdd2e1779
Sujal <sujalkesharwani220@gmail.com>
2024-07-18 23:42:23 +0530

    some pakage added

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,17 @@ import express from "express";
 import dotenv, { config } from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path, { dirname } from "path";
 import { connectDB } from "./db/connectDB.js";
 import messageRouter from "./route/message.route.js";
 import authRouter from "./route/auth.route.js";
 import userRouter from "./route/user.route.js";
 import { app, server } from "./socket/socket.js";
+
+// import { fileURLToPath } from "url";
+// // const __dirname = fileURLToPath(new URL(".", import.meta.url));
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -30,6 +36,12 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
 app.use("/api/users", userRouter);
+
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname,"frontend", "dist", "index.html"));
+// });
 
 server.listen(PORT, async () => {
   console.log(`Server is runs on http://localhost:${PORT}`);
