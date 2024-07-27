1221d75163b81aa596e8f0675f2204978744fa46
Sujal <sujalkesharwani220@gmail.com>
2024-07-27 22:34:33 +0530

    isAuthenticated added according to providers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,26 +8,34 @@ import { useAuthContext } from "./context/AuthContext.jsx";
 import Upload from "./pages/upload/Upload.jsx";
 
 const App = () => {
-  const { authUser } = useAuthContext();
+  const { authUser, isAuthenticated } = useAuthContext();
 
   return (
     <div className="p-4 h-screen flex flex-col items-center justify-center">
       <Routes>
         <Route
           path="/"
-          element={authUser ? <Home /> : <Navigate to="/login" />}
+          element={
+            authUser || isAuthenticated ? <Home /> : <Navigate to="/login" />
+          }
         />
         <Route
           path="/login"
-          element={authUser ? <Navigate to="/" /> : <Login />}
+          element={
+            authUser || isAuthenticated ? <Navigate to="/" /> : <Login />
+          }
         />
         <Route
           path="/signup"
-          element={authUser ? <Navigate to="/" /> : <SignUp />}
+          element={
+            authUser || isAuthenticated ? <Navigate to="/" /> : <SignUp />
+          }
         />
         <Route
           path="/upload"
-          element={authUser ? <Upload /> : <Navigate to="/" />}
+          element={
+            authUser || isAuthenticated ? <Upload /> : <Navigate to="/" />
+          }
         />
       </Routes>
       <Toaster />
