9763e8291be7b15bf6dc95070479b622cf4e8c79
Sujal <sujalkesharwani220@gmail.com>
2024-09-01 14:51:45 +0530

    Home page update

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Sidebar from "../../components/sidebar/Sidebar.jsx";
 import MessageContainer from "../../components/messages/MessageContainer.jsx";
+import Sidebar from "../../components/sidebar/Sidebar.jsx";
 
 const Home = () => {
   return (
