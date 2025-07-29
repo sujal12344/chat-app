dcba9b6c524caf6f4ec3879bdf1e24be569ee839
sujal <sujalkesharwani220@gmail.com>
2025-07-30 00:48:24 +0530

    fixed

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import MessageContainer from "../../components/messages/MessageContainer.jsx";
-import Sidebar from "../../components/Sidebar/Sidebar.jsx";
+import Sidebar from "../../components/sidebar/Sidebar.jsx";
 
 const Home = () => {
   return (
